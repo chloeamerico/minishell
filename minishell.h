3be948cda1c34afc857c1a68575cb7c5c7023ba8
Laurent Leichtnam <lleichtn@f4r14s11.clusters.42paris.fr>
2025-06-02 14:45:04 +0200

    minishell avec le lexer de ton pref

---
+++ minishell.h
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lleichtn <lleichtn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/26 14:21:24 by camerico          #+#    #+#             */
+/*   Updated: 2025/06/02 13:01:36 by lleichtn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+/* ************************************************************************** */
+/* INCLUDES                                                                   */
+/* ************************************************************************** */
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <strings.h>
+# include <unistd.h>
+# include <limits.h>
+# include <stdbool.h>
+# include "./libft/includes/libft.h"
+# include <fcntl.h> 
+# include <sys/wait.h>
+
+/* ************************************************************************** */
+/* DEFINES                                                                    */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/* STRUCTURES                                                                 */
+/* ************************************************************************** */
+
+//struct c'est pour stocker plusieurs variables
+//enum (pour enumeration) est une liste de nom associes a des entiers, par exemple les differents types de token. ce sont tous des int.
+
+//le type du token (a completer)
+typedef enum e_token_type {
+	WORD,			//= 0	//ex : "ls", "fichier.txt"
+	PIPE,			//= 1	//ex : |
+	REDIR_IN,		//= 2	//ex : <
+	REDIR_OUT,		//= 3	//ex : >
+	HEREDOC,				//ex : <<
+	APPEND					//ex : >>
+}	t_token_type;
+
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next; // obligatoire pour chaîner les tokens
+}	t_token;
+
+
+//structure principale de minishell
+typedef struct s_minishell {
+	t_token	*tokens;
+	char	**envp;
+}	t_minishell;
+
+/* ************************************************************************** */
+/* PROTO                                                                      */
+/* ************************************************************************** */
+
+void	create_token(t_token **lst, char *value, t_token_type type);
+void	recup_word(char *input, int *i, t_token **token);
+
+
+
+/* ************************************************************************** */
+/* DEBUG                                                                      */
+/* ************************************************************************** */
+
+#endif
