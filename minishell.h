09a0756daf3d6a922b0084e0c0ebc5f240beb49a
Laurent Leichtnam <lleichtn@f4r14s11.clusters.42paris.fr>
2025-06-02 17:33:27 +0200

    minishell avec le lexer de ton pref

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: lleichtn <lleichtn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 14:21:24 by camerico          #+#    #+#             */
-/*   Updated: 2025/06/02 13:01:36 by lleichtn         ###   ########.fr       */
+/*   Updated: 2025/06/02 15:26:39 by lleichtn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,22 @@ typedef struct s_minishell {
 	t_token	*tokens;
 	char	**envp;
 }	t_minishell;
+
+typedef struct s_redir // stock les fichier a reddiriger
+{
+	char			*file;
+	t_token_type	type; // REDIR_IN, REDIR_OUT, etc.
+	struct s_redir	*next;
+}	t_redir;
+
+typedef struct s_cmd
+{
+	char		**argv;         // arguments de la commande
+	t_redir		*in;            // liste des redirs d’entrée
+	t_redir		*out;           // liste des redirs de sortie
+	struct s_cmd *next;         // commande suivante (si pipe)
+}	t_cmd;
+
 
 /* ************************************************************************** */
 /* PROTO                                                                      */
