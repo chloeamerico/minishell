f0475cef3cf279a6bf364c7d2a145dafa5bc90ce
Laurent Leichtnam <lleichtn@f2r5s19.clusters.42paris.fr>
2025-06-05 17:32:07 +0200

    partie 1 split

--- src/split.c
+++ src/split.c
@@ -85,28 +85,3 @@ char	**word_to_array(char **array, char *word)
 	free(array);
 	return (new_array);
 }
-
-
-//fonction qui va prendre un mot et l'ajouter au tableau array
-char	**word_to_array(char **array, char *word)
-{
-	int	i;
-	char **new_array;
-	
-	i = 0;
-	while(array && array[i])
-		i++;
-	new_array = malloc(sizeof(char *) * (i + 2));	// on ajoute 2 pour garder une place pour le NULL
-	if (!new_array)
-		return (NULL);
-	i = 0;
-	while(array && array[i])
-	{
-		new_array[i] = array[i];	// on duplique array dans new_array
-		i++;
-	}
-	new_array[i] = word;
-	new_array[i + 1] = NULL;
-	free(array);
-	return (new_array);
-}
