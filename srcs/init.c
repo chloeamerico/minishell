0fa2afcca4b805637198505e197ac378512337ab
lolo0292 <105970890+lolo0292@users.noreply.github.com>
2025-06-18 12:20:41 +0200

    Update init.c

--- srcs/init.c
+++ srcs/init.c
@@ -12,22 +12,6 @@
 
 #include "minishell.h"
 
-// Signal envoyer lors de ctrl +C
-static void	sigint_handler(int signum)
-{
-	(void)signum;
-	write(1, "\n", 1);
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
-}
-
-void	setup_signals(void)
-{
-	signal(SIGINT, sigint_handler);
-	signal(SIGQUIT, SIG_IGN);
-}
-
 // char	**init_env(char **envp)
 // {
 // 	int		i;
@@ -60,12 +44,85 @@ void	setup_signals(void)
 // 	return (env_storage);
 // }
 
+// Signal envoyer lors de ctrl +C
+static void	sigint_handler(int signum)
+{
+	(void)signum;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+void	setup_signals(void)
+{
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+static t_env	*create_node(char *envdeb)
+{
+	t_env	*node;
+	char	*equal;
+	size_t	len;
+
+	node = malloc(sizeof(t_env));
+	if (!node)
+		return (NULL);
+	equal = ft_strchr(envdeb, '=');
+	if (!equal)
+	{
+		node->key = ft_strdup(envdeb);
+		node->value = NULL;
+	}
+	else
+	{
+		len = equal - envdeb;
+		node->key = ft_substr(envdeb, 0, len);
+		node->value = ft_strdup(equal + 1);
+	}
+	node->next = NULL;
+	return (node);
+}
+
+t_env	*init_env_list(char **envp)
+{
+	t_env	*head;
+	t_env	*current;
+	t_env	*new;
+
+	head = NULL;
+	current = NULL;
+	while (*envp)
+	{
+		new = create_node(*envp);
+		if (!new)
+			return (NULL);
+		if (!head)
+			head = new;
+		else
+			current->next = new;
+		current = new;
+		envp++;
+	}
+	return (head);
+}
+
+t_env	*get_env_list(t_env *new_env)
+{
+	static t_env	*env = NULL;
+
+	if (new_env)
+		env = new_env;
+	return (env);
+}
+
 int	read_line(char **line)
 {
 	*line = readline("minishell$ "); //met minishell$ sur le bash
 	if (!*line)
 		return (0);
 	if (**line)
-		add_history(*line); //ajout la ligne de commence
+		add_history(*line); //ajout la ligne de commande
 	return (1);
 }
