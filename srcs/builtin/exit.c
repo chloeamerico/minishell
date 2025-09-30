deed9d97c97e15c129fcc5461e9760b98f2f77e5
Laurent Leichtnam <leichtnam2@gmail.com>
2025-09-30 16:36:00 +0200

    echo << l1

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: lleichtn <lleichtn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 11:56:32 by camerico          #+#    #+#             */
-/*   Updated: 2025/09/30 16:30:26 by lleichtn         ###   ########.fr       */
+/*   Updated: 2025/09/30 16:35:53 by lleichtn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,42 +30,6 @@ static int	is_number(char *s)
 	}
 	return (1);
 }
-
-// int	ft_exit(char **args)
-// {
-// 	int	code;
-// 	int	fd_tty;
-
-// 	fd_tty = open("/dev/tty", O_RDWR);
-// 	if (fd_tty)
-// 	{
-// 		write(fd_tty, "exit\n", 5);
-// 		close(fd_tty);
-// 	}
-// 	if (!args[1])
-// 	get_global()->want_exit = 1;
-// 	get_global()->exit_code = 0;
-// 	return (0);
-
-// 	if (!is_number(args[1]))
-// 	{
-// write(2, "exit: numeric argument required\n", 32);
-// get_global()->want_exit = 1;
-// get_global()->exit_code = 2;
-// return (2);
-// 	}
-// 	if (args[2])
-// 	{
-// write(2, "exit: numeric argument required\n", 32);
-// get_global()->want_exit = 1;
-// get_global()->exit_code = 2;
-// return (2);
-// 	}
-// code = ft_atoi(args[1]);
-// get_global()->want_exit = 1;
-// get_global()->exit_code = (unsigned char)code;
-// return ((unsigned char)code);
-// }
 
 // int	ft_exit(char **args)
 // {
