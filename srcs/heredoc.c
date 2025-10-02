dc2abfc74a90d05ec71bae45b7350abf33d75a4c
Laurent Leichtnam <leichtnam2@gmail.com>
2025-10-02 15:51:02 +0200

    correc hd

--- srcs/heredoc.c
+++ srcs/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: lleichtn <lleichtn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 12:51:17 by lleichtn          #+#    #+#             */
-/*   Updated: 2025/10/01 17:24:48 by lleichtn         ###   ########.fr       */
+/*   Updated: 2025/10/02 15:49:16 by lleichtn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,27 +66,11 @@ static int	hd_loop(int wfd, char *delim, int expand, t_env *env)
 	return (0);
 }
 
-// t_env	*hd_env(int op, t_env *val)
-// {
-// 	static t_env	*keep = NULL;
-
-// 	if (op == 0)
-// 		keep = val;
-// 	return (keep);
-// }
-
-// t_cmd	*hd_cmd(int op, t_cmd *val)
-// {
-// 	static t_cmd	*keep = NULL;
-
-// 	if (op == 0)
-// 		keep = val;
-// 	return (keep);
-// }
 static void	hd_sigint_handler(int sig)
 {	
 	t_env	*e;
 	t_cmd	*c;
+
 	(void)sig;
 	e = hd_env(1, NULL);
 	c = hd_cmd(1, NULL);
@@ -97,52 +81,156 @@ static void	hd_sigint_handler(int sig)
 	_exit(130);
 }
 
+static void	hd_sigquit_handler(int sig)
+{	
+	t_env	*e;
+	t_cmd	*c;
+
+	(void)sig;
+	e = hd_env(1, NULL);
+	c = hd_cmd(1, NULL);
+	if (e)
+		free_env(e);
+	if (c)
+		free_cmd_list(c);
+	_exit(131);
+}
+
+// int	ms_heredoc(char *delim, int expand, t_env *env, t_cmd *cmd)
+// {
+// 	int		p[2];
+// 	pid_t	pid;
+// 	int		st;
+// 	void	(*old_int)(int);
+// 	void	(*old_quit)(int);
+
+// 	if (pipe(p) < 0)
+// 		return (-1);
+// 	pid = fork();
+// 	if (pid < 0)
+// 		return (close(p[0]), close(p[1]), -1);
+// 	if (pid == 0)
+// 	{
+// 		hd_env(0, env);
+// 		hd_cmd(0, cmd);
+// 		signal(SIGINT, hd_sigint_handler);
+// 		signal(SIGQUIT, hd_sigquit_handler);
+// 		close(p[0]);
+// 		if (hd_loop(p[1], delim, expand, env))
+// 		{
+// 			close(p[1]);
+// 			free_env(env);
+// 			free_cmd_list(cmd);
+// 			_exit(130);
+// 		}
+// 		close(p[1]);
+// 		free_env(env);
+// 		free_cmd_list(cmd);
+// 		_exit(0);
+// 	}
+// 	close(p[1]);
+// 	old_int = signal(SIGINT, SIG_IGN);
+// 	old_quit = signal(SIGQUIT, SIG_IGN);
+// 	if (waitpid(pid, &st, 0) < 0)
+// 	{
+// 		signal(SIGINT, old_int);
+// 		signal(SIGQUIT, old_quit);
+// 		return (close(p[0]), -1);
+// 	}
+// 	signal(SIGINT, old_int);
+// 	signal(SIGQUIT, old_quit);
+// 	if (WIFEXITED(st) && WEXITSTATUS(st) == 0)
+// 	{
+// 		fcntl(p[0], F_SETFD, FD_CLOEXEC);
+// 		return (p[0]);
+// 	}
+// 	if (WIFEXITED(st) && WEXITSTATUS(st) == 130)
+// 		get_global()->hd_interrupted = 1;
+// 	close(p[0]);
+// 	return (-1);
+// }
+
+static int	ms_heredoc_child_loop(int wfd, char *delim, int expand, t_env *env)
+{
+	if (hd_loop(wfd, delim, expand, env))
+	{
+		close(wfd);
+		free_env(env);
+		return (1);
+	}
+	close(wfd);
+	free_env(env);
+	return (0);
+}
+
+static void	ms_heredoc_child(int p[2], char *delim, int expand, t_env *env, t_cmd *cmd)
+{
+	hd_env(0, env);
+	hd_cmd(0, cmd);
+	signal(SIGINT, hd_sigint_handler);
+	signal(SIGQUIT, hd_sigquit_handler);
+	close(p[0]);
+	if (ms_heredoc_child_loop(p[1], delim, expand, env))
+	{
+		free_cmd_list(cmd);
+		_exit(130);
+	}
+	free_cmd_list(cmd);
+	_exit(0);
+}
+
+static int	ms_heredoc_wait(int p[2], pid_t pid, int *st, void (*old_int)(int), void (*old_quit)(int))
+{
+	int	ret;
+
+	ret = 0;
+	if (waitpid(pid, st, 0) < 0)
+	{
+		signal(SIGINT, old_int);
+		signal(SIGQUIT, old_quit);
+		close(p[0]);
+		return (-1);
+	}
+	signal(SIGINT, old_int);
+	signal(SIGQUIT, old_quit);
+	if (WIFEXITED(*st) && WEXITSTATUS(*st) == 0)
+	{
+		fcntl(p[0], F_SETFD, FD_CLOEXEC);
+		ret = p[0];
+	}
+	else
+	{
+		if (WIFEXITED(*st) && WEXITSTATUS(*st) == 130)
+			get_global()->hd_interrupted = 1;
+		close(p[0]);
+		ret = -1;
+	}
+	return (ret);
+}
+
 int	ms_heredoc(char *delim, int expand, t_env *env, t_cmd *cmd)
 {
-	int		p[2];
-	pid_t	pid;
-	int		st;
-	void	(*old_handler)(int);
+	int			p[2];
+	pid_t		pid;
+	int			st;
+	void		(*old_int)(int);
+	void		(*old_quit)(int);
 
 	if (pipe(p) < 0)
 		return (-1);
 	pid = fork();
 	if (pid < 0)
-		return (close(p[0]), close(p[1]), -1);
-	if (pid == 0)
 	{
-		hd_env(0, env);
-		hd_cmd(0, cmd);
-		signal(SIGINT, hd_sigint_handler);
-		signal(SIGQUIT, SIG_IGN);
 		close(p[0]);
-		if (hd_loop(p[1], delim, expand, env))
-		{
-			close(p[1]);
-			free_env(env);
-			free_cmd_list(cmd);
-			_exit(130);
-		}
+		// close(p[1]);
+		return (-1);
+	}
+	else
 		close(p[1]);
-		free_env(env);
-		free_cmd_list(cmd);
-		_exit(0);
-	}
+	if (pid == 0)
+		ms_heredoc_child(p, delim, expand, env, cmd);
 	close(p[1]);
-	old_handler = signal(SIGINT, SIG_IGN);
-	if (waitpid(pid, &st, 0) < 0)
-	{
-		signal(SIGINT, old_handler);
-		return (close(p[0]), -1);
-	}
-	signal(SIGINT, old_handler);
-	if (WIFEXITED(st) && WEXITSTATUS(st) == 0)
-	{
-		fcntl(p[0], F_SETFD, FD_CLOEXEC);
-		return (p[0]);
-	}
-	if (WIFEXITED(st) && WEXITSTATUS(st) == 130)
-		get_global()->hd_interrupted = 1;
-	close(p[0]);
-	return (-1);
+	old_int = signal(SIGINT, SIG_IGN);
+	old_quit = signal(SIGQUIT, SIG_IGN);
+	return (ms_heredoc_wait(p, pid, &st, old_int, old_quit));
 }
