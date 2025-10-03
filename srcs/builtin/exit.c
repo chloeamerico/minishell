f9abcfcdb5b6081bed0fe52090daf0ffd34675cb
Chloe Americo <camerico@f2r10s12.clusters.42paris.fr>
2025-10-03 15:26:14 +0200

    token.c avance, signal.c ok, init.c ok, exit.c ok

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lleichtn <lleichtn@student.42.fr>          +#+  +:+       +#+        */
+/*   By: camerico <camerico@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 11:56:32 by camerico          #+#    #+#             */
-/*   Updated: 2025/10/01 11:36:32 by lleichtn         ###   ########.fr       */
+/*   Updated: 2025/10/03 14:15:01 by camerico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,64 +31,11 @@ static int	is_number(char *s)
 	return (1);
 }
 
-// int	ft_exit(char **args)
-// {
-// 	int	code;
-// 	int	fd_tty;
-
-// 	fd_tty = open("/dev/tty", O_RDWR);
-// 	if (fd_tty)
-// 	{
-// 		write(fd_tty, "exit\n", 5);
-// 		close(fd_tty);
-// 	}
-// 	if (!args[1])
-// 	get_global()->want_exit = 1;
-// 	get_global()->exit_code = 0;
-// 	return (0);
-
-// 	if (!is_number(args[1]))
-// 	{
-// write(2, "exit: numeric argument required\n", 32);
-// get_global()->want_exit = 1;
-// get_global()->exit_code = 2;
-// return (2);
-// 	}
-// 	if (args[2])
-// 	{
-// write(2, "exit: numeric argument required\n", 32);
-// get_global()->want_exit = 1;
-// get_global()->exit_code = 2;
-// return (2);
-// 	}
-// code = ft_atoi(args[1]);
-// get_global()->want_exit = 1;
-// get_global()->exit_code = (unsigned char)code;
-// return ((unsigned char)code);
-// }
-
-// SANS TTY
-// int	ft_exit(char **args)
-// {
-// 	int	code;
-// 	// int	fd_tty;		 //a remettre quand on remet le tty
-
-// 	write(1, "exit\n", 5);
-// 	if (!args[1])
-// 		exit(0);
-// 	if (!is_number(args[1]))
-// 	{
-// 		write(2, "exit: numeric argument required\n", 32);
-// 		exit(2);
-// 	}
-// 	if (args[2])
-// 	{
-// 		write(2, "exit: too many arguments\n", 26);
-// 		return (1);
-// 	}
-// 	code = ft_atoi(args[1]);
-// 	exit((unsigned char)code);
-// }
+static void	set_exit_code(int code)
+{
+	get_global()->want_exit = 1;
+	get_global()->exit_code = (unsigned char)code;
+}
 
 int	ft_exit(char **args)
 {
@@ -103,22 +50,16 @@ int	ft_exit(char **args)
 	}
 	if (!args[1])
 	{
-		get_global()->want_exit = 1;
-		get_global()->exit_code = 0;
+		set_exit_code(0);
 		return (0);
 	}
 	if (!is_number(args[1]))
 	{
 		write(2, "exit: numeric argument required\n", 32);
-		get_global()->want_exit = 1;
-		get_global()->exit_code = 2;
-		return (2);
+		return (set_exit_code(0), 2);
 	}
 	if (args[2])
-	{
-		write(2, "exit: too many arguments\n", 26);
-		return (1);
-	}
+		return (write(2, "exit: too many arguments\n", 26), 1);
 	code = ft_atoi(args[1]);
 	get_global()->want_exit = 1;
 	get_global()->exit_code = (unsigned char)code;
