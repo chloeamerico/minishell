b98d5e3bd7690ff1c17bbf35247b7b21814fd2cc
Laurent Leichtnam <leichtnam2@gmail.com>
2025-10-04 12:52:51 +0200

    enfinnnnnnnnnnnnn

--- srcs/heredoc.c
+++ srcs/heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: camerico <camerico@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lleichtn <lleichtn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 12:51:17 by lleichtn          #+#    #+#             */
-/*   Updated: 2025/10/02 18:41:38 by camerico         ###   ########.fr       */
+/*   Updated: 2025/10/04 12:52:38 by lleichtn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -276,6 +276,7 @@ int	ms_heredoc(char *delim, int expand, t_env *env, t_cmd *cmd)
 	params.cmd = cmd;
 	if (pipe(p) < 0)
 		return (-1);
+	close(p[1]);
 	pid = fork();
 	if (pid < 0)
 	{
